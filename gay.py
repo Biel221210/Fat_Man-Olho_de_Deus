d3421ca7c8c113da41b60764df3948834609ef7b
Biel221210 <149751756+Biel221210@users.noreply.github.com>
2026-01-21 23:28:12 -0300

    Create gay.py

---
+++ gay.py
@@ -0,0 +1,12 @@
+VERMELHO = "\033[1;91m"
+gay = input(VERMELHO + """
+            [1] O dev do code é gay?
+            [2] Eu sou gay?
+            Escolha: """ + RESET)
+if gay == '1':
+    print("Não. Ele é muito sigma, mais que você inclusive.")
+elif gay == '2':
+    respostas = ["Sim.", "Não.", "Talvez.", "Com certeza!", "De jeito nenhum."]
+    print(random.choice(respostas))
+else:
+    print("Opção inválida.")
