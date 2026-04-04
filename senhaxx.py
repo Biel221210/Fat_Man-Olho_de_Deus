609f0fb627f25857a3efccd0a098bce444a663fb
Biel221210 <149751756+Biel221210@users.noreply.github.com>
2026-04-03 23:54:06 -0400

    Add files via upload

---
+++ senhaxx.py
@@ -0,0 +1,14 @@
+import subprocess
+import getpass
+import sys
+VERDE = '\033[32m'
+VERMELHO = '\033[31m'
+RESET = '\033[0m'
+senha = getpass.getpass(f'Senha: ')
+senha_confirmacao = getpass.getpass(f'Confirme a senha: ')
+if senha == senha_confirmacao:
+    print(VERDE + 'Acesso concedido' + RESET)
+    subprocess.run(['python3', 'olho_de_deus.py'])
+else:
+    print(VERMELHO + 'Acesso negado' + RESET)
+    sys.exit()
