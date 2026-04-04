7334691fe7ebf1f0a6e17830e8ecc2a63b0ae5bb
Biel221210 <149751756+Biel221210@users.noreply.github.com>
2026-04-04 01:05:06 -0300

    Refactor and add password protection to script

--- olho_de_deus.py
+++ olho_de_deus.py
@@ -20,7 +20,19 @@ print("\033[0;32m")
 subprocess.run(['clear'])
 subprocess.run(['cat', 'art_ascii'], stderr=subprocess.DEVNULL)
 
-subprocess.run(['python3', 'senhaxx.py'])
+import getpass
+import sys
+VERDE = '\033[32m'
+VERMELHO = '\033[31m'
+RESET = '\033[0m'
+senha = "220614"
+senha_confirmacao = getpass.getpass(f'senha: ')
+if senha == senha_confirmacao:
+    print(VERDE + 'Acesso concedido' + RESET)
+    subprocess.run(['python3', 'olho_de_deus.py'])
+else:
+    print(VERMELHO + 'Acesso negado' + RESET)
+    sys.exit()
 
 # ==========================================
 # CONFIG UNIVERSAL
